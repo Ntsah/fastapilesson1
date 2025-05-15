a2aa19490174aa80b70c908dff3ced3f2f9794f2
Al <needle_skosam@hotmail.com>
2025-05-16 00:56:27 +0300

    refactordbengine

--- app/backend/db.py
+++ app/backend/db.py
@@ -1,7 +1,16 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import DeclarativeBase
 
-engine = create_async_engine('postgresql+asyncpg://postgres:eminem@localhost:5432/ecommerce', echo=True)
+POSTGRES_USER = 'postgres'
+POSTGRES_PASSWORD = 'eminem'
+POSTGRES_HOST = 'localhost'
+POSTGRES_PORT = 5432
+POSTGRES_DB_NAME = 'ecommerce'
+
+
+engine = create_async_engine(
+    f'postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB_NAME}',
+    echo=True)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 
